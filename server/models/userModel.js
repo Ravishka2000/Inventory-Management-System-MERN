d5a64aa79ecdccf00f522e6408f698f8aa028567
Ravishka Dulshan <92871474+Ravishka2000@users.noreply.github.com>
2023-03-05 22:37:47 +0530

    Initialize project

---
+++ server/models/userModel.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const user = new Schema({
+    name: {
+        type: String,
+        required: [true, "Please Add a Name"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please Add a Email"],
+        unique: true,
+        trim: true,
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            "Please enter a valid email address"
+        ]
+    },
+    password: {
+        type: String,
+        required: [true, "Please add a Password"],
+        minLength: [6, "Password must be upto 6 Characters"],
+        maxLength: [23, "Password must not be more than 23 Characters"],
+    },
+    photo: {
+        type: String,
+        required: [true, "Please add a Photo"]
+    },
+    phone: {
+        type: String,
+        default: "+94"
+    },
+    bio: {
+        type: String,
+        maxLength: [255, "Bio must not be more than 255 Characters"],
+        default: "Bio"
+    }
+}, {
+    timestamps: true,
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
+import userRoute from "./routes/userRoute.js";
 
 dotenv.config();
 
@@ -18,8 +19,10 @@ const URI = process.env.MONGOOSE_URI;
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.use(cors());
-// app.use("/", TodoRoutes);
+app.use("/api/users", userRoute);
 
 mongoose.set("strictQuery", false);
 mongoose.connect(URI, PARAMS)
