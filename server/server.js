c8cb606611363effaee1868afe45ba19702ca075
Ravishka Dulshan <92871474+Ravishka2000@users.noreply.github.com>
2023-03-05 20:59:29 +0530

    Initialize project

---
+++ server/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const PARAMS = {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+};
+
+const URI = process.env.MONGOOSE_URI;
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+// app.use("/", TodoRoutes);
+
+mongoose.set("strictQuery", false);
+mongoose.connect(URI, PARAMS)
+    .then(() => {
+        app.listen(PORT, 
+        () => {
+            console.info(`Server running on PORT ${PORT} 🔥`)
+        })
+    })
+    .catch((err) => {
+        console.error(err.message)
+    });
